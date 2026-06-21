f1e52fbb634eae15a2d1c15a7f9f601433c4220d
p24016574 <p24016574@student.newinti.edu.my>
2026-06-21 17:38:36 +0800

    no

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const adminDashboardRoutes = require('./routes/adminDashboardRoutes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static("public"));
 
 app.use("/api", authRoutes);
 app.use("/api", adminReportsRoutes);
 app.use('/api', adminDashboardRoutes);
-
 
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
